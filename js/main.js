8da5934bc96d1bf96d71e09133e66176a10ba4ec
Dmitry <100037421+milkyway565@users.noreply.github.com>
2022-09-16 19:04:56 +0300

    fix styles

--- js/main.js
+++ js/main.js
@@ -1,17 +1,4 @@
 
-// (function () {
-//   const burgerItem = document.querySelector('.burger');
-//   const menu = document.querySelector('.header__nav');
-//   const menuCloseItem = document.querySelector('.header__nav-close');
-//   burgerItem.addEventListener('click', () => {
-//     menu.classList.add('header__nav_active');
-//   });
-//   menuCloseItem.addEventListener('click', () =>{
-//     menu.classList.remove('header__nav_active')
-//   })
-// }());
-
-
 (function () {
   const burgerItem = document.querySelector('.burger');
   const menu = document.querySelector('.header__nav');
@@ -28,10 +15,16 @@
 }());
 
 
-// (function () {
-//   const popUp = document.querySelector('.header__button');
-//   const m
-// })
-
+$(document).ready(function () {
+  $('.destinations__slider').slick({
+    arrows: false,
+    dots: true,
+    centerMode: true,
+    centerPadding: '160px',
+    slidesToShow: 1,
+    variableWidth: true,
+    
+  });
+});
 
 
